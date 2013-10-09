9343a6476d33d3bb0c3730c905c3d4b602c3ac26
Novi Trisman Hadi <h4dy22@gmail.com>
2013-10-09 21:19:32 +0700

    Praktikum Modul 3 Revisi 1

---
+++ 110533430563/Praktikum 3/login/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Administrator</title>
+</head>
+<body>
+	<?php
+	if(($_POST['user']=='hady') AND ($_POST['pass']=='hady')) {
+	echo '<br><br><center>Selamat Datang di Halaman ini USER ' . $_POST['user'];
+	}
+	else {
+	echo "<br><br><br><body text='red'><strong><center>INVALID LOGIN<br><br><a href='login.html'><h4>Kembali ke Login</h4></a></center></strong></body>";
+	}
+	?>
+</body>
+</html> 
